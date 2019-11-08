269266611a131efce84764277f2514aa4d00e936
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-08 23:48:41 +0300

    Creating a template for urls

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask
 
+import views
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def home_page():
-    return "Hello, world!"
-
+def create_app():
+    app = Flask(__name__)
+    
+    ## Urls for application
+    app.add_url_rule("/", view_func = views.home_page)
+    
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run()
