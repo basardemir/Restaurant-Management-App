e3c54d8eb658225c8f2bc8e5d49e15c3aa30e592
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-09 13:22:56 +0300

    Url Problem
    urls added into main function

--- server.py
+++ server.py
@@ -4,12 +4,6 @@ import views
 
 app = Flask(__name__)
 
-def create_app():
-    ## Urls for application
-    app.add_url_rule("/", view_func = views.home_page)
-    
-    return app
-
 if __name__ == "__main__":
-    app = create_app()
+    app.add_url_rule("/", view_func = views.home_page)
     app.run()
