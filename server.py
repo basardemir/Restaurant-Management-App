a08be04c7f3dc1e7abe7b03eede32df719ef1f46
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-09 13:28:19 +0300

    Url Problem Fixed
    Urls added to global

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask
 import views
 
 app = Flask(__name__)
+app.add_url_rule("/", view_func = views.home_page)
 
 if __name__ == "__main__":
-    app.add_url_rule("/", view_func = views.home_page)
     app.run()
