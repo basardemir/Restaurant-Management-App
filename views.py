dfb22f47f22b839c08eb019244dacad98118a660
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-16 18:25:33 +0300

    internal error fixing

--- views.py
+++ views.py
@@ -1,9 +1,10 @@
 from flask import render_template
-from datetime import datetime
+import datetime
 
 from pages.company import *
 from pages.country import *
 from pages.users import *
+
 def home_page():
   today = datetime.today()
   day_name = today.strftime("%A")
