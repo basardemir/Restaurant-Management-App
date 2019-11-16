4184903209b4f45b046011809a692a47ebb40300
metsey <seyran17@itu.edu.tr>
2019-11-17 01:18:49 +0300

    Imported meal.py

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from flask import render_template
 from pages.company import *
 from pages.country import *
 from pages.users import *
+from pages.meal import *
 
 def home_page():
   return render_template("home.html")
