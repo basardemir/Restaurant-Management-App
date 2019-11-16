cf4b238f573406e89dc6c94867cf2df5e5e638fa
metsey <seyran17@itu.edu.tr>
2019-11-17 01:11:01 +0300

    Added routes to meals page

--- server.py
+++ server.py
@@ -21,5 +21,11 @@ app.add_url_rule("/countries/<int:country_key>/edit", view_func=views.country_up
 app.add_url_rule("/users", view_func=views.users_page)
 app.add_url_rule("/users/create", view_func=views.add_user_page, methods=["GET", "POST"])
 
+#Meals
+app.add_url_rule("/meals", view_func=views.meal_page)
+app.add_url_rule("/meals/food_values", view_func=views.food_value_page)
+app.add_url_rule("/meals/add_meal", view_func=views.add_meal_page)
+
+
 if __name__ == "__main__":
     app.run()
