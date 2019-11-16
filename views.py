4472c5cbb96aa8e77d0496086be08fe2491b17c6
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-16 18:30:28 +0300

    home page edited.

--- views.py
+++ views.py
@@ -1,11 +1,8 @@
 from flask import render_template
-import datetime
 
 from pages.company import *
 from pages.country import *
 from pages.users import *
 
 def home_page():
-  today = datetime.today()
-  day_name = today.strftime("%A")
-  return render_template("home.html", day = day_name)
+  return render_template("home.html")
