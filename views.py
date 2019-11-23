20e01cdc1d59b7826a3a503c3e7e4c4cc1a59f94
DemirUgur <ugurdemir1998@hotmail.com>
2019-11-23 13:06:43 +0300

    error fix

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from datetime import datetime
+from datetime import datetime as dt
 
 from pages.company import *
 from pages.country import *
@@ -7,7 +7,7 @@ from pages.users import *
 from pages.meal import *
 
 def home_page():
-  today = datetime.today()
+  today = dt.today()
   day_name = today.strftime("%A")
   return render_template("home.html",day=day_name)
 
