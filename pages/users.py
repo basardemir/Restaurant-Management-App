161c7f7e077d2a9d88119713c6b99e78fea8a058
iberatkaya <beratkaya1998@gmail.com>
2019-11-26 19:42:34 +0300

    User login form was edited

--- pages/users.py
+++ pages/users.py
@@ -1,5 +1,33 @@
 from flask import Flask, url_for, redirect, request, render_template
+#from passlib.hash import pbkdf2_sha256 as hasher
 import datetime
+from flask import current_app
+from flask_login import UserMixin, LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+lm = LoginManager()
+
+
+@lm.user_loader
+def load_user(user_id):
+    return get_user(user_id)
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.active = True
+        self.is_admin = False
+
+    def get_id(self):
+        return self.username
+
+    @property
+    def is_active(self):
+        return self.active
+
 
 def users_page():
     testusers = [{"name": "Ali", "date": datetime.datetime.now()}]
@@ -15,6 +43,10 @@ def add_user_page():
         print(data)
         return redirect(url_for("users_page"))
 
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
 def signin_page():
     if request.method == "GET":
         print("Add a user")
@@ -22,5 +54,18 @@ def signin_page():
     else:
         print("POSTED:")
         data = {"username": request.form['username'], "password": request.form["password"]}
+        form = LoginForm()
+        if form.validate_on_submit():
+            username = form.data["username"]
+            user = get_user(username)
+            if user is not None:
+                password = form.data["password"]
+                if hasher.verify(password, user.password):
+                    login_user(user)
+                    flash("You have logged in.")
+                    next_page = request.args.get("next", url_for("users_page"))
+                    return redirect(next_page)
+            flash("Invalid credentials.")
+        return render_template("/users/read.html", form=form)
         print(data)
         return redirect(url_for("users_page"))
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 from flask import Flask
-
+from flask_login import LoginManager 
 import views
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config['SECRET_KEY'] = 'db2019'
+
 app.add_url_rule("/", view_func = views.home_page)
 
 app.add_url_rule("/companies", view_func = views.companies_page)
