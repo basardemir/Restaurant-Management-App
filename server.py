3e985e6fc569cd216581651ab5535d8e1e46f480
metsey <seyran17@itu.edu.tr>
2019-11-26 03:15:44 +0300

    Added method option to url rule

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ app.add_url_rule("/users/create", view_func=views.add_user_page, methods=["GET",
 #Meals
 app.add_url_rule("/meals", view_func=views.meal_page)
 app.add_url_rule("/meals/food_values", view_func=views.food_value_page)
-app.add_url_rule("/meals/add_meal", view_func=views.add_meal_page)
+app.add_url_rule("/meals/add_meal", view_func=views.add_meal_page, methods=["GET", "POST"])
 
 
 if __name__ == "__main__":
