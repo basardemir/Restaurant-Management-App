2fa5bc74ae69ce35e22a4739f19a1934df6dea4f
metsey <seyran17@itu.edu.tr>
2019-11-27 01:56:51 +0300

    Implemented search from database, added type column

--- pages/meal.py
+++ pages/meal.py
@@ -11,9 +11,47 @@ def meal_page():
         is_Manager=True
     else:
         is_Manager=False
-    ##Temprorary data
+
     meals = ["Burger", "Fries", "Salad", "Drink", "Desert", "Side Meal"]
-    return render_template("/meals/meals.html", mealss=meals, userType=is_Manager)
+
+    if request.method == "GET":
+        connection=dbapi2.connect(url)
+        cursor = connection.cursor()
+        statement = "select food_name, brand_name, price, isVegan, type from food"
+        cursor.execute(statement)
+        food_table = cursor.fetchall()
+        cursor.close()
+        return render_template("/meals/meals.html", mealss=meals, userType=is_Manager, f_table = food_table)
+    else:
+        search_dict = request.form.to_dict()
+        print(search_dict)
+        if search_dict['searched_meal_name']:
+            s_meal_name = search_dict['searched_meal_name']
+            s_meal_name += "%"
+        else:
+            s_meal_name = search_dict['searched_meal_name']
+            
+        searched_meals = []
+        for i in range(1, len(search_dict)):
+            searched_meals.append(list(search_dict.keys())[i])
+
+        
+
+        connection=dbapi2.connect(url)
+        cursor = connection.cursor()
+        statement = "select food_name, brand_name, price, isVegan, type from food where food_name like %(meal_name)s"
+        for s_m in searched_meals:
+            statement += " or type='"+s_m+"'"
+        statement += ";"
+        print(statement)
+        cursor.execute(statement, {"meal_name": s_meal_name})
+        food_table = cursor.fetchall()
+        cursor.close()
+        return render_template("/meals/meals.html", mealss=meals, userType=is_Manager, f_table = food_table)
+        
+
+    
+    
 
 def food_value_page():
     return render_template("/meals/food_value.html")
@@ -27,19 +65,14 @@ def add_meal_page():
         meal_name = request.form["meal_name"]
         brand_name = request.form["brand_name"]
         price = int(request.form["price"])
-        # meal_option = request.form["meal_option"]
-        vegan = int(request.form["VeganorNot"] == "notvegan_option")
+        meal_option = request.form["meal_option"]
+        vegan = int(request.form["VeganorNot"] == "Vegan")
 
-        # print("meal name: ", meal_name) 
-        # print("brand name: ", brand_name) 
-        # print("price: ", price) 
-        # print("option: ", meal_option) 
-        # print("vegan: ", vegan)
-
+        #db connection and insertion operation
         connection=dbapi2.connect(url)
         cursor = connection.cursor()
-        statement = "insert into FOOD (food_name, brand_name, price, isVegan) VALUES (%(food_name)s, %(brand_name)s, %(price)s, %(vegan)s);"
-        cursor.execute(statement, {'food_name': meal_name, 'brand_name': brand_name, 'price': price, 'vegan': vegan})
+        statement = "insert into FOOD (food_name, brand_name, price, isVegan, type) VALUES (%(food_name)s, %(brand_name)s, %(price)s, %(vegan)s, %(meal_option)s);"
+        cursor.execute(statement, {'food_name': meal_name, 'brand_name': brand_name, 'price': price, 'vegan': vegan, 'meal_option':meal_option})
         connection.commit()
 
         return render_template("/meals/add_meal.html", meal_types = mealTypes) 
