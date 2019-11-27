27d2dafc4ac8e52576eef85e9be3103ffa411acf
Mehmet Can Gün <mehmet-can-gun@hotmail.com>
2019-11-27 09:35:45 +0300

    Company CRUD Operations
    Company create, delete, update and view page simply created with db connection

--- pages/company.py
+++ pages/company.py
@@ -1,17 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for
 
-from flask import Flask, render_template
+from models.company import *
+
+from .forms.company_form import CompanyForm
+
 
 def companies_page():
-  return render_template("/companies/index.html")
+  companies = get_all_companies()
+  return render_template("/companies/index.html", companies = companies)
 
 def company_add_page():
-  return render_template("/companies/create.html")
+  
+  company = CompanyForm()
+  if company.validate_on_submit():
+    company_info = (
+      company.data["name"],
+      company.data["information"],
+      company.data["mission"],
+      company.data["vision"],
+      company.data["abbrevation"],
+      company.data["foundation_date"],
+      company.data["type"]
+    )
+    
+    company_key = add_company(company_info)
+    return redirect(url_for("company_details_page", company_key = company_key))
+  
+  return render_template(
+    "/companies/create.html",
+    form = company
+  )
 
 def company_delete_page(company_key):
+  
   return render_template("/companies/delete.html")
 
 def company_update_page(company_key):
-  return render_template("/companies/update.html")
+  _company = get_company(company_key)
+  company = CompanyForm()
+
+  if company.validate_on_submit():
+    company_info = (
+      company.data["name"],
+      company.data["information"],
+      company.data["mission"],
+      company.data["vision"],
+      company.data["abbrevation"],
+      company.data["foundation_date"],
+      company.data["type"],
+      company_key
+    )
+    return redirect( url_for("company_details_page", company_key = company_key) )
+  
+  company.name.data             = _company["name"]
+  company.information.data      = _company["information"]
+  company.mission.data          = _company["mission"]
+  company.vision.data           = _company["vision"]
+  company.abbrevation.data      = _company["abbrevation"]
+  company.foundation_date.data  = _company["foundation_date"]
+  company.type.data             = _company["type"] if _company["type"] is not None else -1
+  print(_company["type"])
+  return render_template(
+    "/companies/update.html",
+    form = company
+  )
 
 def company_details_page(company_key):
-  return render_template("/companies/details.html")
+  print(company_key)
+  company = get_company(company_key)
+  if(company is None):
+    abort(404)
+  return render_template(
+    "/companies/details.html",
+    company = company
+  )
--- server.py
+++ server.py
@@ -8,11 +8,11 @@ app.config['SECRET_KEY'] = 'db2019'
 
 app.add_url_rule("/", view_func = views.home_page)
 
-app.add_url_rule("/companies", view_func = views.companies_page)
-app.add_url_rule("/companies/create", view_func = views.company_add_page)
-app.add_url_rule("/companies/<int:company_key>", view_func = views.company_details_page)
-app.add_url_rule("/companies/<int:company_key>/edit", view_func = views.company_update_page)
-app.add_url_rule("/companies/<int:company_key>/delete", view_func = views.company_delete_page)
+app.add_url_rule("/companies", view_func = views.companies_page, methods=["GET", "POST"])
+app.add_url_rule("/companies/create", view_func = views.company_add_page, methods=["GET", "POST"])
+app.add_url_rule("/companies/<int:company_key>/details", view_func = views.company_details_page, methods=["GET", "POST"])
+app.add_url_rule("/companies/<int:company_key>/edit", view_func = views.company_update_page, methods=["GET", "POST"])
+app.add_url_rule("/companies/<int:company_key>/delete", view_func = views.company_delete_page, methods=["GET", "POST"])
 
 ##COUNTRY TABLE
 app.add_url_rule("/countries", view_func=views.country_page, methods=["GET", "POST"]) ##Enterance Page to country table
