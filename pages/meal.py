777a5c9f5c23be1d9c1c7273eb96d3b844b5077d
metsey <seyran17@itu.edu.tr>
2019-12-01 02:20:53 +0300

    Changed the url variable

--- pages/meal.py
+++ pages/meal.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2 as dbapi2
-
-url = "postgres://ivpallnyfezioy:075baf8e129b0d52dbd6d87dd3c774363b0b10b499921f821378ed7084bfc744@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/dagmb1jla3rmdp"
+import os
+DB_URL = os.getenv("DATABASE_URL")
 
 def meal_page():
     ##Temprorary data
@@ -15,7 +15,7 @@ def meal_page():
     meals = ["Burger", "Fries", "Salad", "Drink", "Desert", "Side Meal"]
 
     if request.method == "GET":
-        connection=dbapi2.connect(url)
+        connection=dbapi2.connect(DB_URL)
         cursor = connection.cursor()
         statement = "select food_id, food_name, brand_name, price, isVegan, type from food"
         cursor.execute(statement)
@@ -36,7 +36,7 @@ def meal_page():
 
         
 
-        connection=dbapi2.connect(url)
+        connection=dbapi2.connect(DB_URL)
         cursor = connection.cursor()
         statement = "select food_id, food_name, brand_name, price, isVegan, type from food where food_name like %(meal_name)s"
         for s_m in searched_meals:
@@ -48,7 +48,7 @@ def meal_page():
         return render_template("/meals/meals.html", mealss=meals, userType=is_Manager, f_table = food_table)
     
 def food_value_page(food_id):
-    connection=dbapi2.connect(url)
+    connection=dbapi2.connect(DB_URL)
     cursor = connection.cursor()
     statement = "select food_name, calories, carbohydrates, fat, protein, cholesterol from (nutritional_value join food on nutrition_id = nutritional_value_id) where food_id = %(food_id)s;"
     cursor.execute(statement, {'food_id': food_id})
@@ -59,7 +59,7 @@ def food_value_page(food_id):
 def add_meal_page():
     mealTypes = ["Burger", "Fries", "Salad", "Drink", "Desert", "Side Meal"]
     if request.method == "GET":
-        return render_template("/meals/add_meal.html", meal_types = mealTypes)
+        return render_template("/meals/add_meal.html", meal_types = mealTypes, food_props="")
     else:
         meal_name = request.form["meal_name"]
         brand_name = request.form["brand_name"]
@@ -68,7 +68,7 @@ def add_meal_page():
         vegan = int(request.form["VeganorNot"] == "Vegan")
 
         #db connection and insertion operation
-        connection=dbapi2.connect(url)
+        connection=dbapi2.connect(DB_URL)
         cursor = connection.cursor()
         statement = "insert into FOOD (food_name, brand_name, price, isVegan, type) VALUES (%(food_name)s, %(brand_name)s, %(price)s, %(vegan)s, %(meal_option)s);"
         cursor.execute(statement, {'food_name': meal_name, 'brand_name': brand_name, 'price': price, 'vegan': vegan, 'meal_option':meal_option})
@@ -77,7 +77,7 @@ def add_meal_page():
         return redirect(url_for('meal_page')) 
 
 def delete_meal_page(food_id):
-    connection=dbapi2.connect(url)
+    connection=dbapi2.connect(DB_URL)
     cursor = connection.cursor()
     if request.method == "GET":
         statement = "select food_name from food where food_id = %(food_id)s;"
@@ -93,7 +93,7 @@ def delete_meal_page(food_id):
 
 def update_meal_page(food_id):
     mealTypes = ["Burger", "Fries", "Salad", "Drink", "Desert", "Side Meal"]
-    connection = dbapi2.connect(url)
+    connection = dbapi2.connect(DB_URL)
     cursor = connection.cursor()
     if request.method == "GET":
         statement = "select food_name, brand_name, price, isVegan, type from food where food_id = %(food_id)s;"
