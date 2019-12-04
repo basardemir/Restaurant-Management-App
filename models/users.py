114f911273b389f9f6f4af88d7453babbdb53aff
iberatkaya <beratkaya1998@gmail.com>
2019-12-04 21:22:00 +0300

    Fixed SQL errors and added model functions for SQL Queries

--- models/users.py
+++ models/users.py
@@ -5,6 +5,99 @@ import psycopg2 as dbapi2
 import psycopg2.extras
 
 
+#Gets data from the photo table when given the photo_id
+def get_photo_with_id(photo_id): 
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
+            statement = "SELECT * FROM PHOTO WHERE id=%s;"
+            cursor.execute(statement, (photo_id, ))
+            connection.commit()
+            photolist = cursor.fetchall()
+            return photolist
+
+#Updates data from the photo table when given the photo_id
+def update_photo_with_id(photo_id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
+            id = select_a_contactinfo(userid)["id"]
+            statement = "UPDATE PHOTO SET path=%s WHERE id=%s"
+            cursor.execute(statement, (path, photo_id))
+            connection.commit()
+
+#inserts into the photo table when given the dictionary data with attribute photo which contains the image path
+def insert_photo(data):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "INSERT INTO PHOTO (path) VALUES (%s) RETURNING id;"
+            cursor.execute(statement, (data["photo"], ))
+            connection.commit()
+            id = cursor.fetchone()[0]
+            return id
+
+#Deletes a photo row from the photo table when given the id
+def delete_a_photo(id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "DELETE FROM PHOTO WHERE id = %s"
+            cursor.execute(statement, (id, ))
+            connection.commit()
+
+
+#Gets data from the contactinfo table when given the contactinfo_id
+def get_contactinfo_with_id(contactinfo_id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
+            statement = "SELECT * FROM CONTACTINFO WHERE id=%s;"
+            cursor.execute(statement, (contactinfo_id, ))
+            connection.commit()
+            contactinfolist = cursor.fetchall()
+            return contactinfolist
+
+#Updates data from the contactinfo table when given the contactinfo_id
+def update_contactinfo_with_id(contactinfo_id, data):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
+            id = select_a_contactinfo(userid)["id"]
+            statemenryt = "UPDATE CONTACTINFO SET phoneNumber=%s, email=%s, fax=%s, homePhone=%s, workmail=%s WHERE id=%s"
+            cursor.execute(statement, (data["phoneNumber"], data["email"], data["fax"], data["homePhone"], data["workmail"], contactinfo_id))
+            connection.commit()
+
+#Insert into the socialmedia table with the data dictionary
+def insert_socialmedia(data):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "INSERT INTO SOCIALMEDIA (facebook, twitter, instagram, discord, youtube, googleplus) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;"
+            cursor.execute(statement, (data["facebook"], data['twitter'], data["instagram"], data["discord"], data["youtube"], data["googleplus"]))
+            connection.commit()
+            id = cursor.fetchone()[0]
+            return id
+    
+#Insert into the contactinfo table with the data dictionary, and the desired socialmedia_id
+def insert_contactinfo(data, socialmedia_id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "INSERT INTO CONTACTINFO (socialmedia, phoneNumber, email, fax, homePhone, workmail) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;"
+            cursor.execute(statement, (socialmedia_id, data['phoneNumber'], data["email"], data["fax"], data["homePhone"], data["workmail"]))
+            connection.commit()
+            id = cursor.fetchone()[0]
+            return id
+
+#Delete a row from the contactinfo table with the id
+def delete_a_contactinfo(id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "DELETE FROM CONTACTINFO WHERE id = %s"
+            cursor.execute(statement, (id, ))
+            connection.commit()
+
+#Delete a row from the socialmedia table with the id
+def delete_a_socialmedia(id):
+    with dbapi2.connect(DB_URL) as connection:
+        with connection.cursor() as cursor:
+            statement = "DELETE FROM SOCIALMEDIA WHERE id = %s"
+            cursor.execute(statement, (id, ))
+            connection.commit()
+
 def get_user_by_username(username):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor() as cursor:
@@ -19,9 +112,8 @@ def get_user_by_username(username):
 def check_if_user_exists(data):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor() as cursor:
-            #Cannot add the parameter to the execute statement since dbapi2 gives 'TypeError: not all arguments converted during string formatting', this was the only solution
-            statement = "SELECT * FROM USERACCOUNT WHERE username='%s';" % data["username"]
-            cursor.execute(statement)
+            statement = "SELECT * FROM USERACCOUNT WHERE username=%s;"
+            cursor.execute(statement, (data["username"], ))
             connection.commit()
             userlist = cursor.fetchall()
             if userlist == []:
@@ -70,38 +162,20 @@ def select_all_users_and_info():
 def select_a_user_and_info(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM SOCIALMEDIA JOIN (SELECT * FROM CONTACTINFO JOIN (SELECT * FROM (SELECT PERSON.id, contactinfo, name, surname, birthday, educationLevel, gender, path FROM PERSON JOIN PHOTO ON PHOTO.id = PERSON.photo) AS PER JOIN ((SELECT * FROM USERACCOUNT WHERE id = '%s') AS T JOIN MEMBERSHIP ON T.membershiptype = MEMBERSHIP.id) AS T2 ON PER.id = T2.person) AS T3 ON CONTACTINFO.id = T3.contactinfo) AS T4 ON T4.socialmedia = SOCIALMEDIA.id;" % (userid)
-            cursor.execute(statement)
+            statement = "SELECT * FROM SOCIALMEDIA JOIN (SELECT * FROM CONTACTINFO JOIN (SELECT * FROM (SELECT PERSON.id, contactinfo, name, surname, birthday, educationLevel, gender, path FROM PERSON JOIN PHOTO ON PHOTO.id = PERSON.photo) AS PER JOIN ((SELECT * FROM USERACCOUNT WHERE id = %s) AS T JOIN MEMBERSHIP ON T.membershiptype = MEMBERSHIP.id) AS T2 ON PER.id = T2.person) AS T3 ON CONTACTINFO.id = T3.contactinfo) AS T4 ON T4.socialmedia = SOCIALMEDIA.id;"
+            cursor.execute(statement, (userid, ))
             connection.commit()
             userlist = cursor.fetchall()
             cursor.close()
             return userlist
     
 
-def insert_socialmedia(data):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            statement = "INSERT INTO SOCIALMEDIA (facebook, twitter, instagram, discord, youtube, googleplus) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;"
-            cursor.execute(statement, (data["facebook"], data['twitter'], data["instagram"], data["discord"], data["youtube"], data["googleplus"]))
-            connection.commit()
-            id = cursor.fetchone()[0]
-            return id
-    
-def insert_contactinfo(data, socialmedia_id):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            statement = "INSERT INTO CONTACTINFO (socialmedia, phoneNumber, email, fax, homePhone, workmail) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id;"
-            cursor.execute(statement, (socialmedia_id, data['phoneNumber'], data["email"], data["fax"], data["homePhone"], data["workmail"]))
-            connection.commit()
-            id = cursor.fetchone()[0]
-            return id
 
 def check_if_photo_exists(data):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor() as cursor:
-            #Cannot add the parameter to the execute statement since dbapi2 gives 'TypeError: not all arguments converted during string formatting', this was the only solution
-            statement = "SELECT * FROM PHOTO WHERE (path='%s');" % data["photo"]
-            cursor.execute(statement)
+            statement = "SELECT * FROM PHOTO WHERE (path=%s);"
+            cursor.execute(statement, (data["photo"], ))
             connection.commit()
             photolist = cursor.fetchall()
             if photolist == []:
@@ -109,15 +183,6 @@ def check_if_photo_exists(data):
             else:
                 return True
 
-def insert_photo(data):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            #Cannot add the parameter to the execute statement since dbapi2 gives 'TypeError: not all arguments converted during string formatting', this was the only solution
-            statement = "INSERT INTO PHOTO (path) VALUES ('%s') RETURNING id;" % data["photo"]
-            cursor.execute(statement)
-            connection.commit()
-            id = cursor.fetchone()[0]
-            return id
     
 def insert_person(data, contactinfo_id, photo_id):
     with dbapi2.connect(DB_URL) as connection:
@@ -164,8 +229,8 @@ def select_socialmedia():
 def select_a_socialmedia(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM SOCIALMEDIA JOIN (SELECT socialmedia FROM CONTACTINFO JOIN (SELECT contactinfo FROM PERSON JOIN ((SELECT person FROM USERACCOUNT WHERE id='%s')) AS T2 ON PERSON.id = T2.person) AS T3 ON T3.contactinfo = CONTACTINFO.id) AS T4 ON T4.socialmedia = SOCIALMEDIA.id;" % userid
-            cursor.execute(statement, userid)
+            statement = "SELECT * FROM SOCIALMEDIA JOIN (SELECT socialmedia FROM CONTACTINFO JOIN (SELECT contactinfo FROM PERSON JOIN ((SELECT person FROM USERACCOUNT WHERE id=%s)) AS T2 ON PERSON.id = T2.person) AS T3 ON T3.contactinfo = CONTACTINFO.id) AS T4 ON T4.socialmedia = SOCIALMEDIA.id;"
+            cursor.execute(statement, (userid, ))
             connection.commit()
             data = cursor.fetchall()
             return data[0]
@@ -173,8 +238,8 @@ def select_a_socialmedia(userid):
 def select_a_photo(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM PHOTO JOIN (SELECT photo FROM PERSON JOIN (SELECT person FROM USERACCOUNT WHERE id = %s) AS T2 ON T2.person = PERSON.id) AS T1 ON T1.photo = PHOTO.id;" % userid
-            cursor.execute(statement)
+            statement = "SELECT * FROM PHOTO JOIN (SELECT photo FROM PERSON JOIN (SELECT person FROM USERACCOUNT WHERE id = %s) AS T2 ON T2.person = PERSON.id) AS T1 ON T1.photo = PHOTO.id;"
+            cursor.execute(statement, (userid, ))
             connection.commit()
             data = cursor.fetchall()
             return data[0]
@@ -191,8 +256,8 @@ def update_socialmedia(data, userid):
 def select_a_contactinfo(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM CONTACTINFO JOIN (SELECT contactinfo FROM PERSON JOIN ((SELECT person FROM USERACCOUNT WHERE id='%s')) AS T2 ON PERSON.id = T2.person) AS T3 ON T3.contactinfo = CONTACTINFO.id" % userid
-            cursor.execute(statement)
+            statement = "SELECT * FROM CONTACTINFO JOIN (SELECT contactinfo FROM PERSON JOIN ((SELECT person FROM USERACCOUNT WHERE id=%s)) AS T2 ON PERSON.id = T2.person) AS T3 ON T3.contactinfo = CONTACTINFO.id"
+            cursor.execute(statement, (userid, ))
             connection.commit()
             data = cursor.fetchall()
             return data[0]
@@ -209,8 +274,8 @@ def update_contactinfo(data, userid):
 def select_a_person(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM PERSON JOIN (SELECT person FROM USERACCOUNT WHERE id='%s') AS T2 ON PERSON.id = T2.person;" % userid
-            cursor.execute(statement)
+            statement = "SELECT * FROM PERSON JOIN (SELECT person FROM USERACCOUNT WHERE id=%s) AS T2 ON PERSON.id = T2.person;"
+            cursor.execute(statement, (userid, ))
             connection.commit()
             data = cursor.fetchall()
             return data[0]
@@ -227,8 +292,8 @@ def update_person(data, userid):
 def select_a_user(userid):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor(cursor_factory=dbapi2.extras.RealDictCursor) as cursor:
-            statement = "SELECT * FROM USERACCOUNT WHERE id='%s';"  % userid
-            cursor.execute(statement)
+            statement = "SELECT * FROM USERACCOUNT WHERE id=%s;" 
+            cursor.execute(statement, (userid, ))
             connection.commit()
             data = cursor.fetchall()
             return data[0]
@@ -253,36 +318,18 @@ def update_user_lastentry(data, userid):
 def delete_a_person(id):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor() as cursor:
-            statement = "DELETE FROM PERSON WHERE id = %s" % id
-            cursor.execute(statement)
+            statement = "DELETE FROM PERSON WHERE id = %s"
+            cursor.execute(statement, (id, ))
             connection.commit()
             
-def delete_a_photo(id):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            statement = "DELETE FROM PHOTO WHERE id = %s" % id
-            cursor.execute(statement)
-            connection.commit()
 
-def delete_a_contactinfo(id):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            statement = "DELETE FROM CONTACTINFO WHERE id = %s" % id
-            cursor.execute(statement)
-            connection.commit()
 
-def delete_a_socialmedia(id):
-    with dbapi2.connect(DB_URL) as connection:
-        with connection.cursor() as cursor:
-            statement = "DELETE FROM SOCIALMEDIA WHERE id = %s" % id
-            cursor.execute(statement)
-            connection.commit()
 
 def delete_a_user(id):
     with dbapi2.connect(DB_URL) as connection:
         with connection.cursor() as cursor:
-            statement = "DELETE FROM USERACCOUNT WHERE id = %s" % id
-            cursor.execute(statement)
+            statement = "DELETE FROM USERACCOUNT WHERE id = %s"
+            cursor.execute(statement, (id, ))
             connection.commit()
 
 def delete_current_user():
